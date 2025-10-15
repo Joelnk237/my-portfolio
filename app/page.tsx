98f7be38d2f1305b5f630bbda185fec8f4d46483
Joel Ngounouo Kamga <joelgaetan44@gmail.com>
2025-10-15 13:09:34 +0200

    Layout aktualisieren

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@
 
 import HeroSection from './components/HeroSection';
 import SkillsSection from './components/SkillsSection';
+import CompetencesSection from './components/CompetencesSection';
 import ProjectsSection from './components/ProjectsSection';
 import ContactSection from './components/ContactSection';
 
 export default function SimplePortfolio() {
 	return (
-		<main className="min-h-screen bg-gray-900 pt-12">
+		<main className="min-h-screen bg-[#1A1A1A] pt-12 overflow-x-hidden text-white"> {/* <bg-gray-900> */}
 			<HeroSection />
 			<SkillsSection />
+			<CompetencesSection />
 			<ProjectsSection />
 			<ContactSection />
 		</main>
