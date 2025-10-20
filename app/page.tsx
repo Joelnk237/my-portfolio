218d53302d1dc000ccbfe771c025780eedd86f53
Joel Ngounouo Kamga <joelgaetan44@gmail.com>
2025-10-20 23:19:19 +0200

    structure

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,12 @@ import SkillsSection from './components/SkillsSection';
 import CompetencesSection from './components/CompetencesSection';
 import ProjectsSection from './components/ProjectsSection';
 import ContactSection from './components/ContactSection';
+import NavSection from './components/NavSection';
 
 export default function SimplePortfolio() {
 	return (
 		<main className="min-h-screen bg-[#1A1A1A] pt-12 overflow-x-hidden text-white"> {/* <bg-gray-900> */}
+			<NavSection />
 			<HeroSection />
 			<SkillsSection />
 			<CompetencesSection />
