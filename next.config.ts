fe7254376ed1d38a74ff2b452374c09d6a959663
Joel Ngounouo Kamga <joelgaetan44@gmail.com>
2025-11-19 18:25:38 +0100

    update next configuration

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  i18n: {
-    locales: ["de", "en"], // ✅ supported languages
-    defaultLocale: "en",         // ✅ default langage
-  },
 };
 
 export default nextConfig;
