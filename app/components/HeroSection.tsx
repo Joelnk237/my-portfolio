a6c443a144f0cae21cb49ac0f51469b1a5d7d74b
Joel Ngounouo Kamga <joelgaetan44@gmail.com>
2025-11-28 19:54:51 +0100

    fix Menu

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -92,7 +92,7 @@ const [isClient, setIsClient] = useState(false);
 				</div>
 			</div>
 			</div>
-			<div className="absolute bottom-8 inset-x-0 flex flex-col items-center animate-bounce">
+			<div className="absolute bottom-16 inset-x-0 flex flex-col items-center animate-bounce">
 				<span className="text-gray-400 text-sm mb-2 text-center">{/*Scroll to explore*/}{t.hero.instruction}</span>
 				<svg className="w-6 h-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
 					<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
